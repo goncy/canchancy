54dfeb41145c436e6ca76b8d1b38c1b2a338f12d
goncy <gonzalo.pozzo4@gmail.com>
2025-03-11 11:12:16 -0300

    use cache

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,18 @@
 import type {NextConfig} from "next";
 
+import {INFINITE_CACHE} from "next/dist/lib/constants";
+
 const nextConfig: NextConfig = {
   experimental: {
     reactCompiler: true,
     dynamicIO: true,
+    cacheLife: {
+      default: {
+        stale: undefined,
+        revalidate: INFINITE_CACHE,
+        expire: INFINITE_CACHE,
+      },
+    },
   },
   logging: {
     fetches: {
